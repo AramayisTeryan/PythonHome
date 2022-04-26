0ac654e0ae28f244c98602dd4eba7d77d21ffc06
AramayisTeryan <maytas@mail.ru>
2022-04-26 09:53:40 +0400

    lesson

--- git.py
+++ git.py
@@ -1,1 +1,66 @@
-print("barev")
+#------------- 1 ------------
+# dict1 = {
+#     "a": 10,
+#     "b": 20,
+#     "c": 18,
+# }
+# res = sorted(dict1.values())
+# print(res)
+
+
+# dict1 = {
+#     "d":55,
+#     "a":12,
+#     "b":76,
+#     "c":40,
+#     "e":61,
+# }
+# res = sorted(dict1.values())
+# print(res)
+
+
+#------------ 8 -------------
+# word = str(input("mutqagreq bar------>"))
+# mylist = []
+# mydict = {}
+# for i in word:
+#     mylist.append(word.count(i))
+# for x, y in zip(word, mylist):
+#     mydict.setdefault(x,y)
+# print(mydict)
+
+#-------------- 4 ---------------
+
+# dict1 = {
+#     "a":50,
+#     "b":700
+# }
+# dict2 = {
+#     "c":400,
+#      "d":600
+# }
+# dict1.update(dict2)
+# print(dict1)
+
+
+#---------- 2 -----------
+
+# mydict = {}
+# x = "anun"
+# y = "Aramayis"
+# mydict.setdefault(x, y)
+# print(mydict)
+
+#-------- 5 -----------
+
+mydict = {"a":1, "b":2, "c":12}
+
+
+
+
+#---------- 9 ------------
+
+# old_list = [{"key1":"value1"},{},{},{"key1":"value1"},{"key2":"valuee2"}]
+# del old_list[1]
+# del old_list[2]
+# print(old_list)
